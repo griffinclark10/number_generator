edb9aad270e1003a1470c56764e85c78ab93591d
Griffin Clark <56231413+griffinclark10@users.noreply.github.com>
2024-05-14 09:24:56 -0700

    added icon for copy

--- index.php
+++ index.php
@@ -30,7 +30,11 @@
                     </div>
                     <div class="alert alert-danger d-none" id="error-message"></div>
                     <button type="submit" class="btn btn-primary">Generate</button>
-                    <button type="button" class="btn btn-secondary" onclick="copyToClipboard()">Copy to Clipboard</button>
+                    <button type="button" class="btn btn-outline-secondary" style="padding-top: 4px; padding-bottom: 8px;" onclick="copyToClipboard()">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="14" height="16" fill="currentColor" class="bi bi-copy" viewBox="0 0 16 16">
+                            <path fill-rule="evenodd" d="M4 2a2 2 0 0 1 2-2h8a2 2 0 0 1 2 2v8a2 2 0 0 1-2 2H6a2 2 0 0 1-2-2zm2-1a1 1 0 0 0-1 1v8a1 1 0 0 0 1 1h8a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1zM2 5a1 1 0 0 0-1 1v8a1 1 0 0 0 1 1h8a1 1 0 0 0 1-1v-1h1v1a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V6a2 2 0 0 1 2-2h1v1z"/>
+                        </svg>
+                    </button>
                 </form>
                 <div id="result" class="mt-3"></div>
             </div>
